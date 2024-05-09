cbcf03eb9a2fbe935ad6d6ba2939c3d2ec7dba88
SlaterBenelli <cschmitt1977@gmail.com>
2024-05-09 08:42:43 +0200

    Create StartScript.bat

---
+++ scr/StartScript.bat
@@ -0,0 +1,7 @@
+@Echo off
+cls
+Echo Hallo Christian!
+Echo SystemStart >> C:\Users\LGE\OneDrive\Programmieren\LOGs\StartSkript\Log.Log
+:: C:\Users\LGE\OneDrive\Programmieren\LOGs\StartSkript
+echo %DATE% >> C:\Users\LGE\OneDrive\Programmieren\LOGs\StartSkript\Log.Log
+echo %TIME% >> C:\Users\LGE\OneDrive\Programmieren\LOGs\StartSkript\Log.Log
