f4cf58a9b61f96f211f46ee4b75a3a8fc0983582
SlaterBenelli <cschmitt1977@gmail.com>
2024-05-10 14:10:46 +0200

    Create bootscript.bat
    LGE Version

---
+++ scr/bootscript.bat
@@ -0,0 +1,15 @@
+@echo off
+cls
+echo Hallo Christian!
+REM Komentar
+REM shell:startup
+
+echo Startskript fuer LG Win
+REM echo SystemStart >> E:\Programming\LOGs\Start.log
+
+
+echo. >> E:\Programming\LOGs\Start.log
+echo %DATE% %TIME% System Start >> E:\Programming\LOGs\Start.log
+
+pause
+
